45296137918545aefd386201d3389bd079f4bb44
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-09-21 22:49:30 +0600

    learning view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/about/{name}', function ($name) {
+    return view("about",["name"=>$name]);
+});
