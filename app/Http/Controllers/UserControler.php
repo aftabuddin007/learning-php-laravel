3f3e2c71780512bb57a07b034d00cd02d53c6fc5
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-09-22 23:06:15 +0600

    learning controller

---
+++ app/Http/Controllers/UserControler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class UserControler extends Controller
+{
+    function index(){
+    return ('user');
+}
+    function addUser(){
+    return ('Add user please');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserControler;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/about/{name}', function ($name) {
-    return view("about",["name"=>$name]);
-});
+Route::get('/user',[UserControler::class,'index']);
+Route::get('/add-user',[UserControler::class,'addUser']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/home', function () {
+//     return view('home');
+// });
+// Route::get('/about', function () {
+//     return view('about');
+// });
+// Route::get('/about/{name}', function ($name) {
+//     return view("about",["name"=>$name]);
+// });
+
